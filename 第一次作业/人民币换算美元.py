cd28f58ef5b570b2654795e88909c0ce33b4a6b4
eeeeeee493 <121372920+eeeeeee493@users.noreply.github.com>
2024-10-13 13:45:42 +0800

    第一次作业

---
+++ 第一次作业/人民币换算美元.py
@@ -0,0 +1,3 @@
+CNY = eval(input("请输入人民币金额:"))
+USD = CNY/8.356
+print(f"换算为{USD:.2f}美元")
---
+++ 第一次作业/数字月份输出月份缩写.py
@@ -0,0 +1,3 @@
+Month = "janfebmaraprmayjunjulaugsepoctnovdec"
+Num = int(input("请输入数字月份:"))
+print("该月份英文缩写为",Month[Num*3-3:Num*3].title())
